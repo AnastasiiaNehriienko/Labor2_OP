f521872721cc10f89868b8a0fadd46c2892bfdef
Anastasia <Anastasia@DESKTOP-DQAPE0P>
2021-09-22 00:14:34 +0300

    Добавьте файлы проекта.

---
+++ Labor2/Labor2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int main()
+{
+	float x;
+	float A;
+	std::cout << "Enter A:";
+	std::cin >> A;
+	std::cout << "Enter x:";
+	std::cin >> x;
+	if (x < 0)
+		std::cout << "y=" << x;
+	else
+		std::cout << "y=" << A;
+}
